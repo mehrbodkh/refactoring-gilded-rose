3d4c75a87ccf230ced41d34157d5568217c17110
Mehrbod <mehrbod.kh@gmail.com>
2022-10-31 13:20:56 +0100

    ref: refactor base code

--- src/main/kotlin/com/mehrbod/refactor/Item.kt
+++ src/main/kotlin/com/mehrbod/refactor/Item.kt
@@ -5,3 +5,63 @@ open class Item(var name: String, var sellIn: Int, var quality: Int) {
         return this.name + ", " + this.sellIn + ", " + this.quality
     }
 }
+
+fun Item.asAbstractItem(): AbstractItem = when (name) {
+    "Aged Brie" -> AgedBrie(name, sellIn, quality)
+    "Backstage passes to a TAFKAL80ETC concert" -> BackstagePass(name, sellIn, quality)
+    "Sulfuras, Hand of Ragnaros" -> Sulfuras(name, sellIn, quality)
+    else -> UnknownItem(name, sellIn, quality)
+}
+
+abstract class AbstractItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
+    abstract fun update()
+}
+
+class AgedBrie(name: String, sellIn: Int, quality: Int) : AbstractItem(name, sellIn, quality) {
+    override fun update() {
+        sellIn--
+        if (quality < 50) {
+            quality++
+        }
+    }
+}
+
+class BackstagePass(name: String, sellIn: Int, quality: Int) : AbstractItem(name, sellIn, quality) {
+    override fun update() {
+        if (quality < 50) {
+            quality++
+        }
+
+        if (sellIn < 11) {
+            if (quality < 50) {
+                quality++
+            }
+        }
+
+        if (sellIn < 6) {
+            if (quality < 50) {
+                quality++
+            }
+        }
+        sellIn--
+    }
+}
+
+class Sulfuras(name: String, sellIn: Int, quality: Int) : AbstractItem(name, sellIn, quality) {
+    override fun update() {
+        // nothing happens here
+    }
+}
+
+class UnknownItem(name: String, sellIn: Int, quality: Int) : AbstractItem(name, sellIn, quality) {
+    override fun update() {
+        sellIn--
+        if (sellIn < 0) {
+            quality -= 2
+        } else {
+            quality--
+        }
+
+        if (quality < 0) quality = 0
+    }
+}
